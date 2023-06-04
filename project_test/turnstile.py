ae473e252336d482c57f8258aae4f0d3f429782f
Victor Ruiz <vicruiz85@gmail.com>
2023-06-05 01:29:26 +0200

    modified the docs of turnstile.py

--- project_test/turnstile.py
+++ project_test/turnstile.py
@@ -1,5 +1,5 @@
 def turnstile(song, album):
     '''
-    Script to return song from a Turnstile album (capitalized)
+    Script to return song from a Turnstile album (capitalized) and done
     '''
     return f'{song.capitalize()} is a song from the {album} of Turnstile'
